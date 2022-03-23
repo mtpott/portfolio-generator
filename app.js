86e43fca89d4e9dbede33d22d17400e0076f1d83
megan <mtpott23@gmail.com>
2022-03-23 11:35:35 -0400

    formatted HTML template for featured/nonfeatured projects

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 const inquirer = require('inquirer');
 const { trimEnd } = require('lodash');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
@@ -144,5 +144,11 @@ const promptProject = portfolioData => {
     promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('page created! check out index.html in this directory to see it.');
+        });
     });
